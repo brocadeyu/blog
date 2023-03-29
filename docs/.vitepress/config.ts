abbf88d6caac2a271f591afc953cabc66bec4faf
brocadeyu <yjb1725594062@gmail.com>
2023-03-30 01:46:31 +0800

    fix: 修正部署配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base:"/MyBlog",
+  base:"/MyBlog/",
   title: "Brocadeyu's Blog",
   description: "Thoughts & Blogs",
   themeConfig: {
